beea66281edb57dd8aafa3fdb42fd3ede8d6f039
Mahesh Rajendra Bahir <107680212+MaheshB45@users.noreply.github.com>
2023-09-04 11:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,40 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+// Function to download an image using a URL
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const imgElement = new Image();
+    imgElement.src = image.url;
+
+    imgElement.onload = () => {
+      resolve(imgElement);
+    };
+
+    imgElement.onerror = () => {
+      reject(new Error(`Failed to load image's URL: ${image.url}`));
+    };
+  });
+}
+
+// Function to download all images in parallel and display them
+async function downloadAndDisplayImages() {
+  try {
+    const imagePromises = images.map(downloadImage);
+    const downloadedImages = await Promise.all(imagePromises);
+
+    // Clear the previous content of the output div
+    output.innerHTML = '';
+
+    // Append each downloaded image to the output div
+    downloadedImages.forEach((img) => {
+      output.appendChild(img);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Add a click event listener to the button
+btn.addEventListener("click", downloadAndDisplayImages);
